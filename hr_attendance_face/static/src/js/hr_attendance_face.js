e8eb7eb332f4e176042f50958a8a9ea8115cd455
Tran Thanh Phuc <phuc@trobz.com>
2020-01-16 10:19:23 +0700

    [ADD] - ADD function to recognize the face

--- hr_attendance_face/static/src/js/hr_attendance_face.js
+++ hr_attendance_face/static/src/js/hr_attendance_face.js
@@ -12,9 +12,26 @@ odoo.define('hr_attendance_face.kiosk_mode', function (require) {
             return this._super()
         },
 
+        loadLabeledImages: function () {
+          const labels = ['phuc', 'hue', 'uyen']
+          return Promise.all(
+            labels.map(async label => {
+              const descriptions = []
+              for (let i = 1; i <= 2; i++) {
+                const img = await faceapi.fetchImage(`hr_attendance_face/static/src/labelled-images/${label}/${i}.png`)
+                const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+                descriptions.push(detections.descriptor)
+              }
+              return new faceapi.LabeledFaceDescriptors(label, descriptions)
+            })
+          )
+        },
+
+
         start_face_detection_video: function(){
             // Prepare and loads models
             var weight_location = "hr_attendance_face/static/src/js/weights";
+            var self = this;
 
             // Loading the models
             Promise.all([
@@ -22,8 +39,9 @@ odoo.define('hr_attendance_face.kiosk_mode', function (require) {
                 faceapi.nets.faceLandmark68Net.load(weight_location),
                 faceapi.nets.faceRecognitionNet.load(weight_location),
                 faceapi.nets.faceExpressionNet.load(weight_location),
+                faceapi.nets.ssdMobilenetv1.load(weight_location),
 
-            ]).then(function() {
+            ]).then(async function() {
 
                 // Streaming the webcam
                 var video = document.getElementById('attendance-video-face');
@@ -32,6 +50,11 @@ odoo.define('hr_attendance_face.kiosk_mode', function (require) {
                     stream => video.srcObject = stream,
                     err => console.error(err)
                 );
+
+                const labeledfacedescriptor = await self.loadLabeledImages();
+                const facematcher = new faceapi.FaceMatcher(labeledfacedescriptor, 0.6);
+
+                var results = [];
 
                 // Rendering on playing video
                 video.addEventListener('play', function() {
@@ -49,12 +72,25 @@ odoo.define('hr_attendance_face.kiosk_mode', function (require) {
 
                     // Render and detect every 1 secs
                     setInterval(async() => {
-                        const detections = await faceapi.detectAllFaces(video,
-                            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-                            const resizedDetection = faceapi.resizeResults(detections, CanvasDisplaySize);
-                            canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-                            faceapi.draw.drawDetections(canvas, resizedDetection);
-                            faceapi.draw.drawFaceExpressions(canvas, resizedDetection);
+                        const detections = await faceapi.detectAllFaces(video)
+                            .withFaceLandmarks()
+                            .withFaceExpressions()
+                            .withFaceDescriptors();
+
+                        const resizedDetections = faceapi.resizeResults(detections, CanvasDisplaySize);
+                        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+                        faceapi.draw.drawDetections(canvas, resizedDetections);
+                        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+
+                        // Detect the face
+                        results = resizedDetections.map(d => facematcher.findBestMatch(d.descriptor));
+
+                        results.forEach((result, i) => {
+                          const box = resizedDetections[i].detection.box
+                          const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+                          drawBox.draw(canvas)
+                        })
+
                     }, 1000)
                 });
 
